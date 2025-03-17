c76a2f54e3971a278550e5d4f95e8f8fba1a7701
Hirwa-Joric <hirwajoric@gmail.com>
2025-03-17 20:11:17 +0200

    google api optmisaion

--- server.py
+++ server.py
@@ -40,6 +40,30 @@ CHUNK = RATE
 MICROPHONES_DESCRIPTION = []
 FPS = 60.0
 
+###########################
+# Download model, if it doesn't exist
+###########################
+MODEL_URL = "https://www.dropbox.com/s/cq1d7uqg0l28211/example_model.hdf5?dl=1"
+MODEL_PATH = "models/example_model.hdf5"
+print("=====")
+print("2 / 2: Checking model... ")
+print("=====")
+model_filename = "models/example_model.hdf5"
+homesounds_model = Path(model_filename)
+if (not homesounds_model.is_file()):
+    print("Downloading example_model.hdf5 [867MB]: ")
+    wget.download(MODEL_URL, MODEL_PATH)
+
+##############################
+# Load Deep Learning Model
+##############################
+print("Using deep learning model: %s" % (model_filename))
+model = load_model(model_filename)
+graph = tf.get_default_graph()
+
+##############################
+# Setup Audio Callback
+##############################
 
 
 def audio_samples(in_data, frame_count, time_info, status_flags):
